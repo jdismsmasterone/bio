2842d819023f71c7f5e225966ad71ca010c13c7b
jdismsmasterone <iscjuanmendez@gmail.com>
2016-07-04 18:14:26 -0500

    Story 182: Fix benchmarks for speed

--- sixprueba.php
+++ sixprueba.php
@@ -4,7 +4,7 @@
     
 <head>
     <meta charset="utf-8">
-    <title>BIOTECNOLOGIA COMERCIAL</title>
+    <title>BIOTECNOLOGIA22COMERCIAL</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <!-- Optional Bootstrap theme -->
